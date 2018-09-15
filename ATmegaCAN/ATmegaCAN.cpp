745eb747c72d220ed225c04c84b7afa9e0a145e4
Thomas Boland <thomas.boland@outlook.com>
2018-09-15 10:00:24 -0700

    Added ATmegaCAN library

---
+++ ATmegaCAN/ATmegaCAN.cpp
@@ -0,0 +1,10 @@
+/*
+ Name:		ATmegaCAN.cpp
+ Created:	9/15/2018 9:57:54 AM
+ Author:	Thomas Boland
+ Editor:	http://www.visualmicro.com
+*/
+
+#include "ATmegaCAN.h"
+
+
---
+++ ATmegaCAN/ATmegaCAN.h
@@ -0,0 +1,19 @@
+/*
+ Name:		ATmegaCAN.h
+ Created:	9/15/2018 9:57:54 AM
+ Author:	Thomas Boland
+ Editor:	http://www.visualmicro.com
+*/
+
+#ifndef _ATmegaCAN_h
+#define _ATmegaCAN_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+
